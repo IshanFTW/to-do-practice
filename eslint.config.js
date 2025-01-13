3330dab9f1c9715593210ae7a407e0e5fae72bb8
IshanFTW <ishanjindal2003@gmail.com>
2025-01-13 23:40:42 +0530

    added mark as done feature

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,8 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    rules: {
+    "rules" : {
+      "react/prop-types": "off",
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function AddTodo({ inputValue, setInputValue, handleAddTodo }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="To-do's..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>+</button>
+    </div>
+  );
 }
 
-export default App
+function ToDoList({ todos, handleDelete, handleToggle }) {
+  const toDoList = todos.map((toDo) => {
+    return (
+      <li key={toDo.id}>
+        <span style={{ textDecoration: toDo.done ? "line-through" : "none" }}>
+          {toDo.name}
+        </span>
+        <button onClick={() => handleToggle(toDo.id)}>Mark as done</button>
+        <button onClick={() => handleDelete(toDo.id)}>Delete</button>
+      </li>
+    );
+  });
+
+  return <ul>{toDoList}</ul>;
+}
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    const newToDo = {
+      id: uuid(),
+      name: inputValue,
+      done: false,
+    };
+    setTodos((prevTodos) => [...prevTodos, newToDo]);
+    setInputValue("");
+  };
+
+  const handleDelete = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleToggle = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <AddTodo
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        handleAddTodo={handleAddTodo}
+      />
+      <ToDoList 
+        todos={todos}
+        handleDelete={handleDelete}
+        handleToggle={handleToggle} />
+    </div>
+  );
+}
